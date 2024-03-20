631eeb17dd364fa9a18611dd049686b15f28d10f
Rao Nelakurti <kondala.rao.nelakurti@oracle.com>
2024-03-20 20:14:34 +0530

    update files for logging

--- devops_project.tf
+++ devops_project.tf
@@ -1,19 +1,23 @@
-
-
-
-
 resource "oci_devops_project" "devops_project" {
-    #Required
     compartment_id = var.compartment_id
     name = var.project_name
     notification_config {
-        #Required
         topic_id = oci_ons_notification_topic.devops_notification_topic.id
     }
-    #Optional
-    defined_tags = {"foo-namespace.bar-key"= "value"}
     description = var.project_description
     depends_on = [oci_ons_notification_topic.devops_notification_topic]
+}
+
+
+resource "oci_logging_configuration" "example_logging_config" {
+  compartment_id = var.compartment_id
+  name           = "example-logging-config"
+  description    = "Example logging configuration"
+
+  log_sources {
+    service     = "devops"
+    resource_id = oci_devops_project.devops_project.id
+  }
 }
 
 output "project_id" {
