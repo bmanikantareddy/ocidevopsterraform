0931d27f2983b7e65960576578887db2785ff52a
Rao Nelakurti <kondala.rao.nelakurti@oracle.com>
2024-03-23 14:55:32 +0530

    added one stage. will add remaining stage types

--- build_pipeline.tf
+++ build_pipeline.tf
@@ -39,47 +39,20 @@ resource "oci_devops_build_pipeline_stage" "compile_build_pipeline_stage" {
         #Optional
         items {
             #Required
-            connection_type = var.build_pipeline_stage_build_source_collection_items_connection_type
+            connection_type = "DEVOPS_CODE_REPOSITORY"
 
             #Optional
-            branch = var.build_pipeline_stage_build_source_collection_items_branch
-            connection_id = oci_devops_connection.test_connection.id
-            name = var.build_pipeline_stage_build_source_collection_items_name
-            repository_id = oci_artifacts_repository.test_repository.id
-            repository_url = var.build_pipeline_stage_build_source_collection_items_repository_url
+            branch = "main"
+            name = oci_devops_repository.code_repository.name
+            repository_id = oci_devops_repository.code_repository.id
+            repository_url = oci_devops_repository.code_repository.http_url
         }
     }
-    build_spec_file = var.build_pipeline_stage_build_spec_file
-    defined_tags = {"foo-namespace.bar-key"= "value"}
-    deliver_artifact_collection {
+    primary_build_source = oci_devops_repository.code_repository.name
+    build_spec_file = "helidon-storefront-full/yaml/build/build_spec.yaml"
+    description = "compile ,package ,build docker and push to artifact repo"
+    display_name = "build_docker"
+    image = "OL7_X86_64_STANDARD_10"
 
-        #Optional
-        items {
-
-            #Optional
-            artifact_id = oci_devops_artifact.test_artifact.id
-            artifact_name = var.build_pipeline_stage_deliver_artifact_collection_items_artifact_name
-        }
-    }
-    deploy_pipeline_id = oci_devops_deploy_pipeline.test_deploy_pipeline.id
-    description = var.build_pipeline_stage_description
-    display_name = var.build_pipeline_stage_display_name
-    freeform_tags = {"bar-key"= "value"}
-    image = var.build_pipeline_stage_image
-    is_pass_all_parameters_enabled = var.build_pipeline_stage_is_pass_all_parameters_enabled
-    primary_build_source = var.build_pipeline_stage_primary_build_source
-    private_access_config {
-        #Required
-        network_channel_type = var.build_pipeline_stage_private_access_config_network_channel_type
-        subnet_id = oci_core_subnet.test_subnet.id
-
-        #Optional
-        nsg_ids = var.build_pipeline_stage_private_access_config_nsg_ids
-    }
-    stage_execution_timeout_in_seconds = var.build_pipeline_stage_stage_execution_timeout_in_seconds
-    wait_criteria {
-        #Required
-        wait_duration = var.build_pipeline_stage_wait_criteria_wait_duration
-        wait_type = var.build_pipeline_stage_wait_criteria_wait_type
-    }
+    stage_execution_timeout_in_seconds = 36000
 }
