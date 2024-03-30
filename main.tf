1ee8d908647cf9a7cab6ec0a3001518e217d1534
Rao Nelakurti <kondala.rao.nelakurti@oracle.com>
2024-03-30 21:16:29 +0530

    update modules structure

--- main.tf
+++ main.tf
@@ -1,13 +1,17 @@
 module "prereq" {
-  source = "./preRequisites "  # Path to your module directory or module registry URL
-  
+  source = "./tfmodules/pre_requisites"  # Path to your module directory or module registry URL
   // Pass variables to the module
+  tenancy_ocid = var.tenancy_ocid
   compartment_id = var.compartment_id
+  user_ocid = var.user_ocid
+
 }
 
 module "pipeline" {
-  source = "./ociDevOps"  # Path to your module directory or module registry URL
+  source = "./tfmodules/oci_devops_project"  # Path to your module directory or module registry URL
   
   // Pass variables to the module
+#   tenancy_ocid = var.tenancy_ocid
   compartment_id = var.compartment_id
+#   user_ocid = var.user_ocid
 }
--- tfmodules/pre_requisites/main.tf
+++ tfmodules/pre_requisites/main.tf
@@ -64,9 +64,6 @@ resource "oci_identity_auth_token" "auth_token" {
 
 
 ############# Knoweledge Base ###############
-variable "knowledge_base_display_name" {
-  default = "KB_FSRE_TOOLS"
-}
 resource "oci_adm_knowledge_base" "knowledge_base" {
     compartment_id = var.compartment_id
     display_name = var.knowledge_base_display_name
